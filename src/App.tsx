493f8acedd9cc36a53f9545c26cd1322ec9f8372
María <91290073+MaGaBoo@users.noreply.github.com>
2023-03-05 14:48:48 +0100

    Added component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Counter from './bases/Counter';
 
 function App() {
   return (
     <div className="App">
       <>
       <h1>React</h1>
+      <hr/>
+      <Counter />
       </>
     </div>
   );
